fc963eb38640f094da4d876da3294c3d73936816
dsolsman <danny@dannysolsman.com>
2021-12-27 22:53:15 -0500

    Just getting my feet wet again. Doing automate the boring stuff as a refresher.

--- flowcontrolv2.py
+++ flowcontrolv2.py
@@ -9,4 +9,31 @@ spam = True
 42 == 42
 42 == 99
 2 != 3
-# ok, i'm understanding pycharm a little better, let's move on to writing a script
+# ok, i'm understanding pycharm a little better, let's move on to writing a script, if example
+# print('Please enter your name')
+# name = input()
+# if name =='Danny':
+#    print('Hi, Danny')
+# code prints 'Hi, Danny' if Danny name is entered, nothing if anything else is entered or nothing.
+# adding else
+print('Please enter your name')
+name = input()
+if name =='Danny':
+    print('Hi, Danny')
+else:
+    print('Hello, stranger')
+#if and elif example, coming back to this tomorrow.
+
+name = input()
+if name == 'Alice':
+    print('Hi, Alice.')
+if name == 'Danny':
+    print('Hello Danny')
+print('What is your age?')
+age = input()
+if age < 12:
+    print ('Yes you are a youngin')
+if age > 13 < 3000
+    print ('You are a growing vampire.')
+if age > 3000:
+    print ('Yes you are an undead one of many years ' + name )
