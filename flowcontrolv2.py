df0650cce738c563a9653d96f8d331b73c0fe6c7
dsolsman <danny@dannysolsman.com>
2021-12-27 22:15:32 -0500

    Just getting my feet wet again. Doing automate the boring stuff as a refresher.

---
+++ flowcontrolv2.py
@@ -0,0 +1,12 @@
+# moving from mu-editor back into py-charm professional with vim plugin
+# just checking a few things with pycharm functionality here
+print('Hello World')
+print(7 + 7)
+# on to the lesson and chapter 2
+# booleans lesson
+# testing this in console first
+spam = True
+42 == 42
+42 == 99
+2 != 3
+# ok, i'm understanding pycharm a little better, let's move on to writing a script
