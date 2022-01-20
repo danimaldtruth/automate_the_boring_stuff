7326de0cfbeb53bc80828fefa1333126af2e59d4
dsolsman <danny@dannysolsman.com>
2022-01-20 18:40:37 -0500

    fixed this script with indentation issues and 1 typo

--- part_1-python_basics/ch02-flow_control/rock_paper_scissors.py
+++ part_1-python_basics/ch02-flow_control/rock_paper_scissors.py
@@ -13,49 +13,53 @@ while True: # the main game loop
         print('Enter your move: (r)ock (p)aper (s)cissors or (q)uit')
         playerMove = input()
         if playerMove == 'q':
-            sys.exit() # Quit the prgram
+            sys.exit() # Quit the program
         if playerMove == 'r' or playerMove == 'p' or playerMove == 's':
             break # Break out of the player input loop.
         print('Type one of r, p, s, or q.')
 
         # Display what the player chose:
-        if playerMove == 'r':
-            print('ROCK versus...')
-        elif playerMove == 'p':
-            print('PAPER versus...')
-        elif playerMove == 's':
-            print('SCISSORS versus...')
+    if playerMove == 'r':
+        print('ROCK versus...')
+    elif playerMove == 'p':
+        print('PAPER versus...')
+    elif playerMove == 's':
+        print('SCISSORS versus...')
 
         # Display what the computer chose:
-        randomNumber  = random.randint(1,3)
-        if randomNumber == 1:
-            computerMove = 'r'
-            print('ROCK')
-        elif randomNumber == 2:
-            computerMove = 'p'
-            print('PAPER')
-        elif randomNumber == 3:
-            computerMove = 's'
-            print('SCISSORS')
+    randomNumber  = random.randint(1,3)
+    if randomNumber == 1:
+        computerMove = 'r'
+        print('ROCK')
+    elif randomNumber == 2:
+        computerMove = 'p'
+        print('PAPER')
+    elif randomNumber == 3:
+        computerMove = 's'
+        print('SCISSORS')
 
 # Display and record the win/loss/tie
-        if playerMove == computerMove:
-            print('It is a tie!')
-            ties = ties +_1
-        elif playerMove == 'r' and computerMove == 's':
-            print('You win!')
-            wins = wins + 1
-        elif playerMove == 'p' and computerMove == 'r':
-            print('You win!')
-            wins = wins + 1
-        elif playerMove == 's' and computerMove == 'p':
-            print('You win!')
-            wins = wins + 1
-        elif playerMove == 'r' and computerMove == 'p':
-            print('You lose!')
-            losses = losses + 1
-        elif playerMove == 'p' and computerMove == 's':
-            print('You lose!')
-            losses = losses + 1
+    if playerMove == computerMove:
+        print('It is a tie!')
+        ties = ties + 1
+    elif playerMove == 'r' and computerMove == 's':
+        print('You win!')
+        wins = wins + 1
+    elif playerMove == 'p' and computerMove == 'r':
+        print('You win!')
+        wins = wins + 1
+    elif playerMove == 's' and computerMove == 'p':
+        print('You win!')
+        wins = wins + 1
+    elif playerMove == 'r' and computerMove == 'p':
+        print('You lose!')
+        losses = losses + 1
+    elif playerMove == 'p' and computerMove == 's':
+        print('You lose!')
+        losses = losses + 1
 
 #will go over during python meeting as to why its not running the game
+
+#issue was just an indentation problem as well as a type _ instead of space " "
+
+#next time will add these notes on commit messages instead of comments
