5fb95637237d8732b32cdc467434a47ed4b69749
Danny Solsman <danny@dannysolsman.com>
2022-07-04 15:40:37 -0400

    commiting again

---
+++ misc/misc.py
@@ -0,0 +1,8 @@
+#reviewing old python basics
+name = "danny"
+my_length = len(name)
+print("your name is " + name + ".")
+print("the letter length of your name is " + str(my_length) + " letters long.")
+print("what is your favorite food?")
+favorite_food = input()
+print("Ah very well.. I like eatting " + favorite_food + " too, usually in moderation.")
